bbe0d2fb9846a581506673f328581b7a54cf4e69
Adrien El Zein <adrizein@gmail.com>
2016-11-12 00:52:04 +0100

    chore(setup): create package with setup tools

---
+++ jsontemplate/__init__.py
@@ -0,0 +1,2 @@
+from .template import *
+from .features import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+from setuptools import setup
+
+setup(name='jsontemplate',
+        version='0.1',
+        description='Package to define json template in Python',
+        author='Adrien El Zein',
+        author_email='adrizein@gmail.com',
+        url='https://github.com/adrizein/jsontemplate',
+        license='MIT',
+        packages=['jsontemplate'],
+        )
